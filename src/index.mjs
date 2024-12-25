9933e1e4bad219e3ef50206d41a66520e7d226f8
osama seid <osaseid1@gmail.com>
2024-12-25 15:19:20 +0300

    get method

--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.get('/api/send', (req, res) => {
+    res.status(200).json({ "message" : " Hello World"});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
